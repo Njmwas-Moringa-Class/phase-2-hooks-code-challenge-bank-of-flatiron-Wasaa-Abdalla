10e53594c87bd2feaa6f32e80418712362022b7c
abdalla-wasaa <abdallawasaa7@gmail.com>
2023-12-19 22:50:58 +0300

    Attempted

--- src/components/AddTransactionForm.js
+++ src/components/AddTransactionForm.js
@@ -1,14 +1,85 @@
-import React from "react";
+import React, { useState } from "react";
 
-function AddTransactionForm() {
+function AddTransactionForm({ onTransactionAdded }) {
+  const [formData, setFormData] = useState({
+    date: "",
+    description: "",
+    category: "",
+    amount: 0,
+  });
+
+  const handleInputChange = (e) => {
+    const { name, value } = e.target;
+    setFormData({
+      ...formData,
+      [name]: value,
+    });
+  };
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    
+
+    try {
+      const response = await fetch("http://localhost:8001/transactions", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify(formData),
+      });
+
+      if (response.ok) {
+        // Clear the form data
+        setFormData({
+          date: "",
+          description: "",
+          category: "",
+          amount: 0,
+        });
+
+        // Notify the parent component that a transaction was added
+        onTransactionAdded();
+      } else {
+        console.error("Failed to add transaction.");
+      }
+    } catch (error) {
+      console.error("Error adding transaction:", error);
+    }
+  };
+
   return (
     <div className="ui segment">
-      <form className="ui form">
+      <form className="ui form" onSubmit={handleSubmit}>
         <div className="inline fields">
-          <input type="date" name="date" />
-          <input type="text" name="description" placeholder="Description" />
-          <input type="text" name="category" placeholder="Category" />
-          <input type="number" name="amount" placeholder="Amount" step="0.01" />
+          <input
+            type="date"
+            name="date"
+            value={formData.date}
+            onChange={handleInputChange}
+          />
+          <input
+            type="text"
+            name="description"
+            placeholder="Description"
+            value={formData.description}
+            onChange={handleInputChange}
+          />
+          <input
+            type="text"
+            name="category"
+            placeholder="Category"
+            value={formData.category}
+            onChange={handleInputChange}
+          />
+          <input
+            type="number"
+            name="amount"
+            placeholder="Amount"
+            step="0.01"
+            value={formData.amount}
+            onChange={handleInputChange}
+          />
         </div>
         <button className="ui button" type="submit">
           Add Transaction
--- src/components/TransactionsList.js
+++ src/components/TransactionsList.js
@@ -1,28 +1,125 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import Transaction from "./Transaction";
 
 function TransactionsList() {
+  const [transactions, setTransactions] = useState([]);
+  const [newTransaction, setNewTransaction] = useState({
+    date: "",
+    description: "",
+    category: "",
+    amount: 0,
+  });
+
+  useEffect(() => {
+    // Fetch transaction data from the API endpoint
+    fetch("http://localhost:8001/transactions")
+      .then((response) => response.json())
+      .then((data) => {
+        // Assuming the API response is an array of transactions
+        setTransactions(data);
+      })
+      .catch((error) => {
+        console.error("Error fetching transaction data:", error);
+      });
+  }, []);
+
+  const handleInputChange = (e) => {
+    const { name, value } = e.target;
+    setNewTransaction({
+      ...newTransaction,
+      [name]: value,
+    });
+  };
+
+  const addTransaction = (e) => {
+    e.preventDefault();
+    // Assuming you have some validation logic here
+    // before adding the new transaction
+    if (newTransaction.date && newTransaction.description && newTransaction.category && newTransaction.amount > 0) {
+      setTransactions([...transactions, newTransaction]);
+      setNewTransaction({
+        date: "",
+        description: "",
+        category: "",
+        amount: 0,
+      });
+    }
+  };
+
   return (
-    <table className="ui celled striped padded table">
-      <tbody>
-        <tr>
-          <th>
-            <h3 className="ui center aligned header">Date</h3>
-          </th>
-          <th>
-            <h3 className="ui center aligned header">Description</h3>
-          </th>
-          <th>
-            <h3 className="ui center aligned header">Category</h3>
-          </th>
-          <th>
-            <h3 className="ui center aligned header">Amount</h3>
-          </th>
-        </tr>
-        {/* render a list of <Transaction> components here */}
-      </tbody>
-    </table>
+    <div>
+      <table className="ui celled striped padded table">
+        <tbody>
+          <tr>
+            <th>
+              <h3 className="ui center aligned header">Date</h3>
+            </th>
+            <th>
+              <h3 className="ui center aligned header">Description</h3>
+            </th>
+            <th>
+              <h3 className="ui center aligned header">Category</h3>
+            </th>
+            <th>
+              <h3 className="ui center aligned header">Amount</h3>
+            </th>
+          </tr>
+          {transactions.map((transaction, index) => (
+            <Transaction
+              key={index}
+              date={transaction.date}
+              description={transaction.description}
+              category={transaction.category}
+              amount={transaction.amount}
+            />
+          ))}
+        </tbody>
+      </table>
+      
+      {/* Form for adding new transactions */}
+      <form onSubmit={addTransaction}>
+        <h2>Add New Transaction</h2>
+        <div>
+          <label>Date:</label>
+          <input
+            type="text"
+            name="date"
+            value={newTransaction.date}
+            onChange={handleInputChange}
+          />
+        </div>
+        <div>
+          <label>Description:</label>
+          <input
+            type="text"
+            name="description"
+            value={newTransaction.description}
+            onChange={handleInputChange}
+          />
+        </div>
+        <div>
+          <label>Category:</label>
+          <input
+            type="text"
+            name="category"
+            value={newTransaction.category}
+            onChange={handleInputChange}
+          />
+        </div>
+        <div>
+          <label>Amount:</label>
+          <input
+            type="number"
+            name="amount"
+            value={newTransaction.amount}
+            onChange={handleInputChange}
+          />
+        </div>
+        <button type="submit">Add Transaction</button>
+      </form>
+    </div>
   );
 }
 
 export default TransactionsList;
+
